a34688fad36dc9bbb3ee968070d60fadf95c38cd
raqueldebiase <raqueldebiase@gmail.com>
2022-03-09 22:34:02 +0000

    Add: main.js

--- main.js
+++ main.js
@@ -1,24 +1,23 @@
-document.getElementById("mouseEvent").addEventListener("mouseenter", mouseEnter);
-document.getElementById("mouseEvent").addEventListener("mouseleave", mouseleave);
 
-function mouseEnter (){
-    document.getElementById("mouseEvent").style.borderTop = "4px solid indianred";
+function load() {
+    const test = document.querySelectorAll(".card");
+    console.log(test);
+    test.forEach( e => {
+        e.addEventListener("mouseenter", entrar);
+        e.addEventListener("mouseleave", sair)
+    });
 }
 
-function mouseleave (){
-    document.getElementById("mouseEvent").style.borderTop= "4px solid rgba(35, 166, 202, 0.651)";
-} 
-    
+function entrar (){
+    const el = event.target || event.srcElement;
+    const id = el.id;
+    document.getElementById(id).style.borderTop = "4px solid indianred";
+}
 
-// const mouseEnter = document.querySelectorAll('#mouseEvent');
-
-// mouseEnter.forEach(element => {
-//     element.style.border = "3px solid gray";
-// })
-
-// const mouseLeave = document.querySelectorAll('#mouseEvent');
-
-// mouseLeave.forEach(element => {
-//     element.style.border = "1px solid rgba(141, 148, 150, 0.651)";
-// })
+function sair(){
+    const el = event.target || event.srcElement;
+    const id = el.id;
+    document.getElementById(id).style.borderTop = "4px solid rgba(35, 166, 202, 0.651)";
+}
+document.addEventListener("DOMContentLoaded", load, false);
 
